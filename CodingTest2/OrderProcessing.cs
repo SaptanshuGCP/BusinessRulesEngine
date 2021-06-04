ee9356f83b9106b1d5a2f709a71c31dfab04bffe
SaptanshuGCP <sbhattacharyagcpuser1@gmail.com>
2021-06-04 10:59:12 +0530

    Initial Commit

---
+++ CodingTest2/OrderProcessing.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CodingTest2
+{
+
+    public class PhysicalOrder
+    { 
+        public bool ProcessOrder()
+        {
+        return true;
+        }
+    }
+
+}
---
+++ CodingTest2Tests/OrderProcessingTests.cs
@@ -0,0 +1,41 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodingTest2
+{
+    [TestClass]
+    public class OrderProcessingTests
+    {
+        [TestMethod]
+        public void ProcessPhysical()
+        {
+            PhysicalOrder orderProcess = new PhysicalOrder();
+            bool expectedResult = true;
+            bool actualResult = orderProcess.ProcessOrder();
+            Assert.IsTrue(expectedResult == actualResult);
+        }
+
+        [TestMethod]
+        public void ProcessBook()
+        {
+            
+        }
+
+        [TestMethod]
+        public void ProcessMembership()
+        {
+            
+        }
+
+        [TestMethod]
+        public void ProcessMembershipUpgrade()
+        {
+            
+        }
+
+        [TestMethod]
+        public void ProcessVideo()
+        {
+            
+        }
+    }
+}
