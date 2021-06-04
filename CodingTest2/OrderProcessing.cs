0c0c9c74d812bec866cd628b3c0b226701d77fb5
SaptanshuGCP <sbhattacharyagcpuser1@gmail.com>
2021-06-04 11:20:08 +0530

    Adding the logic to process physical order.

--- CodingTest2/OrderProcessing.cs
+++ CodingTest2/OrderProcessing.cs
@@ -1,14 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CodingTest2
 {
 
-    public class PhysicalOrder
-    { 
-        public bool ProcessOrder()
-        {
-        return true;
-        }
+    public interface IOrder
+    {
+        bool ProcessOrder();
     }
 
+    public interface IRule
+    {
+        bool ProcessRule();
+    }
+
+    public class PhysicalOrder : IOrder
+    {
+        public List<IRule> rules { get; set; }
+        public bool ProcessOrder()
+        {
+            bool ruleProcessed = false;
+            foreach (IRule rule in rules)
+            {
+                ruleProcessed = rule.ProcessRule();
+                if (!ruleProcessed)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
 }
