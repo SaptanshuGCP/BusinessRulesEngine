32fbd609743283c62d10b492e400b36be23a8c55
SaptanshuGCP <sbhattacharyagcpuser1@gmail.com>
2021-06-04 12:07:00 +0530

    Refactor the PhysicalOrder class and induce Dependency Inversion.

--- CodingTest2/OrderProcessing.cs
+++ CodingTest2/OrderProcessing.cs
@@ -14,13 +14,35 @@ namespace CodingTest2
         bool ProcessRule();
     }
 
-    public class PhysicalOrder : IOrder
+    public class GeneratePackingSlip : IRule
     {
-        public List<IRule> rules { get; set; }
-        public bool ProcessOrder()
+        public bool ProcessRule()
+        {
+            Console.WriteLine("Packing slip generated.");
+            return true;
+        }
+    }
+
+    public class CommissionPayment : IRule
+    {
+        public bool ProcessRule()
+        {
+            Console.WriteLine("Commission payment generated.");
+            return true;
+        }
+    }
+
+    public class OrderPayment
+    {
+        List<IRule> _ruleList;
+        public OrderPayment(List<IRule> rules)
+        {
+            rules = _ruleList;
+        }
+        public bool ProcessPaymentRules()
         {
             bool ruleProcessed = false;
-            foreach (IRule rule in rules)
+            foreach (IRule rule in _ruleList)
             {
                 ruleProcessed = rule.ProcessRule();
                 if (!ruleProcessed)
@@ -29,6 +51,18 @@ namespace CodingTest2
                 }
             }
             return true;
+        }
+    }
+
+    public class PhysicalOrder : IOrder
+    {
+        List<IRule> _rules = new List<IRule>();
+        public bool ProcessOrder()
+        {
+            _rules.Add(new GeneratePackingSlip());
+            _rules.Add(new CommissionPayment());
+            OrderPayment order = new OrderPayment(_rules);
+            return order.ProcessPaymentRules();
         }
     }
 
